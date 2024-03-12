f37c3985e92a20dee642247b8083c919e810b78a
Espen Kvernstad <espen.kvernstad@outlook.com>
2024-03-12 18:57:51 +0100

    add database automap from schema to model in sqlalchemy

---
+++ api/app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from .config import config
+
+Base = automap_base()
+
+engine = create_engine(config.database_url)
+
+Base.prepare(engine, reflect=True)
+
+# Define the models
+Hospital = Base.classes.hospitals
+Airport = Base.classes.airports
+SarBase = Base.classes.sar_bases
+
+session = Session(engine)
