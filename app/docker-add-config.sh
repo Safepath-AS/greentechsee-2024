da065c82dc8567e49a672eb6245aebc1742202d5
Per Christian Kvalvik <pc@kvalvik.no>
2024-03-12 09:11:27 +0100

    Add API url to configuration

---
+++ app/docker-add-config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+INDEX_HTML=/usr/share/nginx/html/index.html
+
+sed - i e 's,__API_URL__,'"$API_URL"',' $INDEX_HTML
+
+exec "$@"
---
+++ app/src/serverConfig.ts
@@ -0,0 +1,25 @@
+interface ServerConfig {
+  apiUrl: string;
+}
+
+interface CustomWindow extends Window {
+  serverConfig: ServerConfig;
+}
+
+declare const window: CustomWindow;
+
+const defaultConfig: ServerConfig = {
+  apiUrl: import.meta.env.VITE_API_URL ?? "http://localhost:8000",
+};
+
+export const serverConfig: ServerConfig = window.serverConfig;
+
+const preReplaceConfig: ServerConfig = {
+  apiUrl: "__API_URL__",
+};
+
+for (const key of Object.keys(serverConfig) as Array<keyof ServerConfig>) {
+  if (preReplaceConfig[key] === serverConfig[key]) {
+    serverConfig[key] = defaultConfig[key];
+  }
+}
